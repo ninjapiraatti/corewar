51978dab778369232e1e3c98a030d33146345b93
Ninjapiraatti <tuoppil@gmail.com>
2021-01-15 12:42:14 +0200

    Added a link to readme and tweaked header file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# corewar
+# Hive Helsinki: Corewar
+C — Creating an assembler and a virtual machine.
+
+## Useful reading
+[Good read on a similar project](https://justinmeiners.github.io/lc3-vm/)\
--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -3,16 +3,16 @@
 #include "op.h"
 #include "libft.h"
 
-typedef struct		    s_statement
+typedef struct			s_statement
 {
 	char				*label;
 	int					code;
 	int					cycles;
 	char				*arg[3];
-    int             	type[3];
+	int					type[3];
 	struct s_statement	*next;
 
-}                       t_statement;
+}						t_statement;
 
 
 
