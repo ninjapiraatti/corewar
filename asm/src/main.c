04af9d58d45a9654274daa9b8d07e9b565acb122
Ninjapiraatti <tuoppil@gmail.com>
2021-01-15 14:06:27 +0200

    Created testfiles and some basic read/write stuff

--- asm/src/main.c
+++ asm/src/main.c
@@ -1,19 +1,42 @@
-#include "includes/op.h"
+//#include "includes/op.h"
 #include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int	main(int argc, char **argv)
 {
     char    *file;
     int     fd;
+    //int     fdr;
+    int     i;
+    char    buf[9];
 
     if (argc > 1)
     {
-        file = argv[argc - 1];
+        FILE *fp;
+        //file = argv[argc - 1];
         fd = open(argv[1], O_RDONLY);
+        fp = fopen ("output.cor","w");
+        printf("fd is %d", fd);
+        while ((i = read(fd, buf, 8)) > 0)
+        {
+            /*
+            buf[i] = '\0';
+            if (lem->input == NULL)
+                lem->input = ft_strnew(0);
+            tmp = ft_strjoin(lem->input, buf);
+            ft_strdel(&lem->input);
+            lem->input = tmp;
+            */
+            write(fp, 0x24, 1);
+            printf(buf);
+        }
+        /*
         if (file[ft_strlen(file) - 1] == 's' && file[ft_strlen(file) - 2] == '.')
         {
-            ft_printf("Lollotiloo");
+            printf("Lollotiloo");
         }
+        */
     }
 	return (0);
 }
