16afb76f1d6d9325437ec7dcce7076f520eb2ca1
saraarmiida <sara.pentti@hotmail.fi>
2021-01-25 11:00:25 +0200

    helper functions moved to their own file

--- asm/src/validate.c
+++ asm/src/validate.c
@@ -1,57 +1,14 @@
 #include "../includes/asm.h"
 
-int	ft_strclen(char const *s, int i, char c)
-{
-	int len;
+/*
+** Validates name and comment length and syntax, ignores whitespace
+** and removes comments from the ends of the lines.
+*/
 
-	len = 0;
-	while (s[i] != c && s[i] != '\0')
-	{
-		len++;
-		i++;
-	}
-	return (len);
-}
-
-char	*ft_strcdup(const char *str, char c)
-{
-	char	*dup;
-	size_t	i;
-
-	i = 0;
-	if (!(dup = ft_strnew(ft_strclen(str, i, c))))
-		return (NULL);
-	while (str[i] && str[i] != c)
-	{
-		dup[i] = str[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
-
-char	*ft_strcdup_2d(char **file, int i, int j, char c)
-{
-	char	*dup;
-	char	*new;
-
-	dup = ft_strcdup(&file[i][j], c);
-	j += ft_strlen(dup);
-	while (file[i][j] == '\0')
-	{
-		i++;
-		dup = free_strjoin(dup, ft_strdup("\n"));
-		new = ft_strcdup(file[i], '"');
-		dup = free_strjoin(dup, new);
-		j = ft_strlen(new);
-	}
-	return (dup);
-}
-
-int		validate_st(char **file, int i, char *cmd_string, int length)
+int		validate_st(char **file, int i, char *cmd_string, int max_length)
 {
 	int		j;
-	char	*str;
+	int		len;
 
 	j = ft_strlen(cmd_string);
 	while (file[i][j] != '\0' && ft_isspace(file[i][j]))
@@ -59,24 +16,78 @@ int		validate_st(char **file, int i, char *cmd_string, int length)
 	if (file[i][j] != '"')
 		error_management("error in champion statement");
 	j++;
-	str = ft_strcdup_2d(file, i, j, '"');
-	if ((int)ft_strlen(str) > length)
+	len = 0;
+	while (file[i] != NULL && file[i][j] != '"')
+	{
+		len++;
+		while (file[i][j++] != '\0' && file[i][j] != '"')
+			len++;
+		if (file[i][j] == '"')
+			break ;
+		j = 0;
+		i++;
+	}
+	if (len > max_length)
 		error_management("champion statement length exceeded");
-	ft_printf("statement: '%s'\n", str);
+	if (file[i] == NULL || file[i][j] != '"')
+		error_management("missing double quote on statement");
+	j++;
+	while (file[i][j] != '\0' && ft_isspace(file[i][j]))
+		j++;
+	if (file[i][j] == '#')
+		remove_comment(ft_strchr(file[i], '#'));
+	else if (file[i][j] != '\0')
+		error_management("syntax error in statement");
+	return (i + 1);
+}
+
+/*
+** Validates header (champion name and comment)
+** Checks that there is one and only one name and comment given
+** at the start of the file. Comments, extra whitespace and empty
+** lines are allowed, everything else leads to an error.
+** The order of name and comment doesn't matter and they have to
+** prefixed by the string defined in op.h
+*/
+
+int		validate_header(t_asm *assm)
+{
+	int	i;
+	int	j;
+	int	name;
+	int comment;
+
+	i = 0;
+	j = 0;
+	name = 0;
+	comment = 0;
+	while (assm->file[i] != NULL && (name == 0 || comment == 0))
+	{
+		while (assm->file[i][j] != '\0' && ft_isspace(assm->file[i][j]))
+			j++;
+		if (ft_strncmp(assm->file[i], NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)) == 0)
+		{
+			i = validate_st(assm->file, i, NAME_CMD_STRING, PROG_NAME_LENGTH);
+			name++;
+		}
+		else if (ft_strncmp(assm->file[i], COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING)) == 0)
+		{
+			i = validate_st(assm->file, i, COMMENT_CMD_STRING, COMMENT_LENGTH);
+			comment++;
+		}
+		else if (assm->file[i][j] == '#')
+			i++;
+		else
+			error_management("error in header");
+	}
+	if (name != 1 || comment != 1)
+		error_management("duplicate statement");
 	return (i);
 }
 
-void	remove_comment(char *file)
-{
-	int	i;
-
-	i = 0;
-	while (file[i] != '\0')
-	{
-		file[i] = ' ';
-		i++;
-	}
-}
+/*
+** Removes comments from after header.
+*/
 
 void	validate_file(t_asm *assm)
 {
@@ -84,29 +95,13 @@ void	validate_file(t_asm *assm)
 	int		i;
 
 	file = assm->file;
-	i = 0;
-	while (file[i] != NULL)
-	{
-		ft_printf("%s\n", file[i]);
-		i++;
-	}
-	i = 0;
-	ft_printf("\n\n");
+	i = validate_header(assm);
+	ft_printf("\ni: %d\n\n", i);
+	print_file(file);
 	while (file[i] != NULL)
 	{
 		if (ft_strchr(file[i], '#') != NULL)
 			remove_comment(ft_strchr(file[i], '#'));
-		if (ft_strncmp(file[i], NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)) == 0)
-			i = validate_st(file, i, NAME_CMD_STRING, PROG_NAME_LENGTH);
-		else if (ft_strncmp(file[i], COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING)) == 0)
-			i = validate_st(file, i, COMMENT_CMD_STRING, COMMENT_LENGTH);
-		i++;
-	}
-	i = 0;
-	ft_printf("\n\n");
-	while (file[i] != NULL)
-	{
-		ft_printf("%s\n", file[i]);
 		i++;
 	}
 }
