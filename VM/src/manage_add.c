876861dbca203b14a04ac835087742057bad2a8a
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-02-17 15:14:58 +0200

    commited files which stood in way of a pull request

---
+++ VM/src/manage_add.c
@@ -0,0 +1,19 @@
+# include "vm.h"
+
+// init wait time to -1
+
+void    manage_add(t_carriage *carr, t_arena *arena)
+{
+    int     arg1;
+    int     arg2;
+    int     arg3;
+
+    arg1 = carr->regs[arena[carr->pc + 2].ar - 1];
+    arg2 = carr->regs[arena[carr->pc + 3].ar - 1];
+    arg3 = arg1 + arg2;
+    if (arg3 == 0)
+        carr->carry = true;
+    else
+        carr->carry = false;
+    carr->regs[arena[carr->pc + 4].ar - 1] = arg3;
+}
