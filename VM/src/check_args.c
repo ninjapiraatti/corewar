db03cba0a8652b96ae4eee028951aa2779b7a9e8
saraarmiida <sara.pentti@hotmail.fi>
2021-03-04 11:10:23 +0200

    added debug prints to check_args

--- VM/src/check_args.c
+++ VM/src/check_args.c
@@ -66,6 +66,10 @@ int     check_code(int inst, unsigned char arg_code, t_carriage *c, t_arena *a)
     c->arg_size[0] = get_arg_size(c->args[0], inst);
 	c->arg_size[1] = get_arg_size(c->args[1], inst);
 	c->arg_size[2] = get_arg_size(c->args[2], inst);
+    // useful for debugging
+    // ft_printf("arg1: %d size: %d\n", c->args[0], c->arg_size[0]);
+    // ft_printf("arg2: %d size: %d\n", c->args[1], c->arg_size[1]);
+    // ft_printf("arg3: %d size: %d\n", c->args[2], c->arg_size[2]);
     c->next_state = 2 + c->arg_size[0] + c->arg_size[1] + c->arg_size[2];
     while (i < 3)
     {
