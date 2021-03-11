887e735bf733802777d2f7c5524dd7ea75b1b4f3
saraarmiida <sara.pentti@hotmail.fi>
2021-03-11 18:13:58 +0200

    asm bug fix related to issues with changing to 1d file in validate_header

--- asm/src/validate.c
+++ asm/src/validate.c
@@ -67,23 +67,34 @@ int		validate_header(char *file)
 	comment = 0;
 	while (file[i] != '\0' && (name == 0 || comment == 0))
 	{
+		// ft_printf("at %.10s ", &file[i]);
 		i += skip_spaces(&file[i]);
 		if (!ft_strncmp(&file[i], NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)))
 		{
 			i += ft_strlen(NAME_CMD_STRING);
 			i = validate_st(file, i, PROG_NAME_LENGTH);
+			// ft_printf("found name\n");
 			name++;
 		}
 		else if (!ft_strncmp(&file[i], COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING)))
 		{
 			i += ft_strlen(COMMENT_CMD_STRING);
 			i = validate_st(file, i, COMMENT_LENGTH);
+			// ft_printf("found player comment\n");
 			comment++;
 		}
 		else if (file[i] == COMMENT_CHAR || file[i] == ALT_COMMENT_CHAR)
+		{
 			i += skip_comment(ft_str2chr(&file[i], COMMENT_CHAR, ALT_COMMENT_CHAR));
+			// ft_printf("found comment\n");
+		}
+		else if (file[i] == '\n')
+			i++;
 		else
+		{
+			// ft_printf("found nothing\n");
 			error_management("error in header");
+		}
 	}
 	if (name != 1 || comment != 1)
 		error_management("statement duplicate or missing");
